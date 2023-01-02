8203cd6d0a415c655a0e0d5402ac2dfe1f6b7e17
Abhishek Shah <abhishekshah2905@gmail.com>
2023-01-02 11:05:52 +0530

    Initian commit

---
+++ src/main/java/com/poc/banking/BankingRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.poc.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingRepositoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/poc/banking/BankingRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
